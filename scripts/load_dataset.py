ecc36780b14aabc717ad8bfea2586d11591078f8
TevenLeScao <teven.lescao@gmail.com>
2021-09-29 03:31:26 +0200

    HF datasets in load_dataset, some additional arguments

--- scripts/load_dataset.py
+++ scripts/load_dataset.py
@@ -13,6 +13,7 @@
 
 import tensorflow_datasets as tfds
 import tensorflow as tf
+import datasets
 import os
 import struct
 import numpy as np
@@ -25,8 +26,12 @@ parser = argparse.ArgumentParser(description='Load a dataset.')
 parser.add_argument('--data_dir', type=str)
 parser.add_argument('--save_dir', type=str)
 parser.add_argument('--name', type=str)
+parser.add_argument('--dataset_format', type=str, default="tfds")
 parser.add_argument('--split', type=str)
+parser.add_argument('--hf_ds_subset', type=str, default=None)
 parser.add_argument('--tokenize', action='store_true')
+parser.add_argument('--text_feature_key', type=str, default="text")
+parser.add_argument('--num_workers', type=int, default=1)
 
 args = parser.parse_args()
 
@@ -37,12 +42,21 @@ split = args.split
 data_dir = args.data_dir
 save_dir = args.save_dir
 dataset_name = args.name
+dataset_format = args.dataset_format
+key = args.text_feature_key
+num_workers = args.num_workers
 
 
-ds = tfds.load(dataset_name, split=split, shuffle_files=False, batch_size=2**16,
-               data_dir=data_dir)
-assert isinstance(ds, tf.data.Dataset)
-print(ds)
+if dataset_format == "tfds":
+    ds = tfds.load(dataset_name, split=split, shuffle_files=False, batch_size=2**16,
+                   data_dir=data_dir)
+    assert isinstance(ds, tf.data.Dataset)
+    print(ds)
+elif dataset_format == "hf":
+    subset = args.hf_ds_subset
+    ds = datasets.load_dataset(dataset_name, subset, split=split)
+    assert isinstance(ds, datasets.Dataset), "This is not a HF-dataset. It might be a DatasetDict. Try passing `split`?"
+    print(ds)
 
 UID = 0
 def sep():
@@ -52,24 +66,35 @@ def sep():
 
 def tok(x):
     if args.tokenize:
-        out = tokenizer.encode(x.decode("utf8"))
+        if dataset_format == "tfds":
+            x = x.numpy().decode("utf8")
+        out = tokenizer.encode(x)
         out = np.array(out, dtype=np.uint16).view(np.uint8).tobytes()
     else:
-        out = x
+        if dataset_format == "hf":
+            x = x.encode("utf8")
+        out = x.numpy()
     return out
 
 
+os.makedirs(save_dir, exist_ok=True)
 fout = open(os.path.join(save_dir, dataset_name+"."+split), "wb")
 
-p = mp.Pool(96)
+if num_workers > 1:
+    p = mp.Pool(96)
+else:
+    p = None
 
 i = 0
 sizes = [0]
 for b in ds:
     print(i)
 
-    text = b['text'].numpy()
-    text = p.map(tok,text)
+    text = b[key]
+    if num_workers > 1:
+        text = p.map(tok,text)
+    else:
+        text = map(tok, text)
     
     for x in text:
         next_line = sep()+x
